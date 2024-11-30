ac87cf060138fc007e6252904eec5e717d1ceb17
ansha <alexeyshamrin@mail.ru>
2024-11-30 17:19:16 +0300

    delete

--- dz 6.9/app.js
+++
@@ -1,6 +0,0 @@
-const hasLicence = true;
-const age = 25;
-const isDrink = false;
-
-const canDrive = hasLicence && age >= 18 && !isDrink;
-console.log(`Может водить: ${canDrive ? 'Может' : 'Не может'}`);
